4d278efb02fcd2dbfaf7580d92bf3277955c6253
Elias Le Boudec <contact@eliasleboudec.ch>
2024-11-20 15:03:03 +0100

    validation cartesian to spherical

--- main.py
+++ main.py
@@ -480,20 +480,20 @@ def find_optimal_parameters(meep=None, case=None, max_order=2, cartesian=True, p
 
 
 def main():
-    eps = 4
-    # find_optimal_parameters(meep=f"sim_results_200_res_-1_eps_{eps}", case=f"eps_{eps}",
-    #                         plot_during=True,
-    #                         comsol_direct=True, max_order=2, cartesian=True,
-    #                         compensate_symmetry=False, print_solution=True)
-    # find_optimal_parameters(meep=f"sim_results_200_res_-1_eps_{eps}", case=f"eps_{eps}",
-    #                         plot_during=True,
-    #                         comsol_direct=True, max_order=2, cartesian=False,
-    #                         compensate_symmetry=False, print_solution=True)
-    # eps = 10_000
-    # find_optimal_parameters(meep=f"sim_results_200_res_-1_eps_{eps}", case=f"eps_{eps}",
-    #                         plot_during=True,
-    #                         comsol_direct=True, max_order=2, cartesian=False,
-    #                         compensate_symmetry=False, print_solution=True)
+    eps = 8
+    find_optimal_parameters(meep=f"sim_results_200_res_-1_eps_{eps}", case=f"eps_{eps}",
+                            plot_during=True,
+                            comsol_direct=True, max_order=2, cartesian=True,
+                            compensate_symmetry=False, print_solution=True)
+    find_optimal_parameters(meep=f"sim_results_200_res_-1_eps_{eps}", case=f"eps_{eps}",
+                            plot_during=True,
+                            comsol_direct=True, max_order=2, cartesian=False,
+                            compensate_symmetry=False, print_solution=True)
+    eps = 10_000
+    find_optimal_parameters(meep=f"sim_results_200_res_-1_eps_{eps}", case=f"eps_{eps}",
+                            plot_during=True,
+                            comsol_direct=True, max_order=2, cartesian=False,
+                            compensate_symmetry=False, print_solution=True)
     plot_all_cartesian_results(1, f"mathematica/eps_{eps}")
     plot_all_spherical_results(1, f"mathematica/eps_{eps}")
     plt.show()
